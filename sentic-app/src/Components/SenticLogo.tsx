b18213b48a2bc7bd96fde79ca6e44446a143cd93
lwtopsps <s2300069@cymcac.edu.hk>
2026-06-02 18:56:23 +0800

    Ensure fonts are included

--- sentic-app/src/Components/SenticLogo.tsx
+++ sentic-app/src/Components/SenticLogo.tsx
@@ -1,21 +1,4 @@
-import localFont from 'next/font/local';
 import type { HTMLAttributes } from 'react';
-
-// Using the exact path to your app/fonts folder
-const editorsNote = localFont({
-  src: [
-    {
-      path: '../../app/fonts/EditorsNote-Bold.otf',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: '../../app/fonts/EditorsNote-BoldItalic.otf',
-      weight: '700',
-      style: 'italic',
-    }
-  ]
-});
 
 type SenticLogoProps = HTMLAttributes<HTMLSpanElement> & {
   className?: string;
@@ -25,9 +8,12 @@ export default function SenticLogo({ className = '', ...props }: SenticLogoProps
   return (
     <span
       {...props}
-      className={`${editorsNote.className} ${className}`.trim()}
-      // Notice we removed the dark color so it can be white/light in the sports theme
-      style={{ ...props.style, fontWeight: 700 }}
+      className={className}
+      style={{
+        ...props.style,
+        fontWeight: 700,
+        fontFamily: 'Georgia, Times New Roman, serif',
+      }}
     >
       <span style={{ fontStyle: 'normal' }}>S</span>
       <span style={{ fontStyle: 'italic' }}>ENTI</span>
